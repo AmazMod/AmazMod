2ec33c1659917124ae037ef6dc0e3a68506e9208
lfom <lfom@gmx.com.br>
2018-07-03 22:00:29 -0300

    Updated Preferences Strings (added 2 missing too) and XML, cleaned some code in recents changes and implemented a (dirty) "keep" notification when using Custom UI. ;)

--- service/src/main/java/com/edotassi/amazmodcompanionservice/ui/NotificationActivity.java
+++ service/src/main/java/com/edotassi/amazmodcompanionservice/ui/NotificationActivity.java
@@ -1,12 +1,15 @@
 package com.edotassi.amazmodcompanionservice.ui;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
@@ -16,7 +19,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.edotassi.amazmodcompanionservice.Constants;
 import com.edotassi.amazmodcompanionservice.R;
@@ -29,7 +31,6 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import amazmod.com.models.Reply;
@@ -53,11 +54,14 @@ public class NotificationActivity extends Activity {
     private Handler handler;
     private ActivityFinishRunnable activityFinishRunnable;
 
+    private Boolean nullError = false;
+
     private NotificationData notificationSpec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                 WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
@@ -83,13 +87,16 @@ public class NotificationActivity extends Activity {
 
             if (notificationSpec.getVibration() > 0) {
                 Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-                vibrator.vibrate(notificationSpec.getVibration());
+                if(vibrator != null) {
+                    vibrator.vibrate(notificationSpec.getVibration());
+                }
             }
-        } catch (Exception ex) {
-            Log.d("NotificationActivity","Exception: " + ex + " notificationSpec: " + notificationSpec);
+        } catch (NullPointerException ex) {
+            System.out.println("NotificationActivity onCreate - Exception: " + ex + " notificationSpec: " + notificationSpec);
             title.setText("AmazMod");
             text.setText("Welcome to AmazMod");
-            icon.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher_background));
+            icon.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.amazmod));
+            nullError = true;
         }
 
         handler = new Handler();
@@ -105,7 +112,7 @@ public class NotificationActivity extends Activity {
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
 
-        if (!disableNotificationReplies) {
+        if ((!disableNotificationReplies) && (!nullError)) {
 
             List<Reply> repliesList = loadReplies();
             for (final Reply reply : repliesList) {
@@ -142,15 +149,20 @@ public class NotificationActivity extends Activity {
 
     @OnClick(R2.id.activity_notification_button_reply)
     public void clickReply() {
-        Toast.makeText(this, "not_implented", Toast.LENGTH_SHORT).show();
+        if (nullError) {
+            finish();
+        }
+        else {
+//            Toast.makeText(this, "not_implented", Toast.LENGTH_SHORT).show();
+            postWithStandardUI(notificationSpec);
+            finish();
+        }
     }
 
     private void startTimerFinish() {
-        try {
-            handler.removeCallbacks(activityFinishRunnable);
+        handler.removeCallbacks(activityFinishRunnable);
+        if (!nullError) {
             handler.postDelayed(activityFinishRunnable, notificationSpec.getTimeoutRelock());
-        } catch (Exception ex) {
-        Log.d("NotificationActivity","Exception: " + ex + " activityFinishRunnable: " + activityFinishRunnable);
         }
     }
 
@@ -172,4 +184,32 @@ public class NotificationActivity extends Activity {
             return new ArrayList<>();
         }
     }
+
+    private void postWithStandardUI(NotificationData notificationData) {
+
+        NotificationManager notificationManager;
+        Context context;
+
+        context=getBaseContext();
+        notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        Log.d("NotifIcon", "2");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "")
+                .setSmallIcon(android.R.drawable.ic_dialog_email)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                .setStyle(new NotificationCompat.InboxStyle())
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(notificationData.getText()))
+                .setContentText(notificationData.getText())
+                .setContentTitle(notificationData.getTitle())
+                .setVibrate(new long[]{0});
+
+        Notification notification = builder.build();
+        try {
+            notificationManager.notify(notificationData.getId(), notification);
+        } catch (NullPointerException ex) {
+            System.out.println("NotificationActivity postWithStandarUI - Exception: " + ex + " notification: " + notification);
+        }
+    }
+
+
 }
