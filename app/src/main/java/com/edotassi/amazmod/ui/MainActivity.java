d78408615957290459d93c760eaf8eb61349420d
LFOM <lfom@gmx.com.br>
2018-08-08 04:44:38 -0300

    Better handling of HEB in MaincActivity It avoids some NullPointerExceptions in logcat, but it is still creating duplicated cards (fragments) when the activity is created...

--- app/src/main/java/com/edotassi/amazmod/ui/MainActivity.java
+++ app/src/main/java/com/edotassi/amazmod/ui/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.LayoutInflaterCompat;
 import android.support.design.widget.NavigationView;
