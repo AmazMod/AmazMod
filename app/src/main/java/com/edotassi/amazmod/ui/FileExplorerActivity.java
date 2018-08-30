4e6a0e4d21ff0c46f5e74ea30c89c1651a6e967d
Edoardo Tassinari <edo.tassinari@gmail.com>
2018-08-30 14:42:58 +0200

    fixed some issue in FileExplorer

--- app/src/main/java/com/edotassi/amazmod/ui/FileExplorerActivity.java
+++ app/src/main/java/com/edotassi/amazmod/ui/FileExplorerActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ListView;
 
@@ -11,12 +12,14 @@ import com.edotassi.amazmod.R;
 import com.edotassi.amazmod.adapters.FileExplorerAdapter;
 import com.edotassi.amazmod.event.Directory;
 import com.edotassi.amazmod.watch.Watch;
+import com.google.android.gms.common.util.Strings;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import amazmod.com.transport.data.DirectoryData;
@@ -93,6 +96,22 @@ public class FileExplorerActivity extends AppCompatActivity {
                                 String jsonFiles = directoryData.getFiles();
                                 List<FileData> filesData = gson.fromJson(jsonFiles, new TypeToken<List<FileData>>() {
                                 }.getType());
+
+                                if (!path.equals("/")) {
+                                    FileData parentDirectory = new FileData();
+                                    parentDirectory.setName("..");
+                                    parentDirectory.setDirectory(true);
+
+                                    String[] pathComponents = path.split("/");
+                                    String parentPath = TextUtils.join("/", Arrays.copyOf(pathComponents, pathComponents.length - 1));
+                                    if (Strings.isEmptyOrWhitespace(parentPath)) {
+                                        parentPath = "/";
+                                    }
+
+                                    parentDirectory.setPath(parentPath);
+
+                                    filesData.add(0, parentDirectory);
+                                }
 
                                 fileExplorerAdapter.clear();
                                 fileExplorerAdapter.addAll(filesData);
