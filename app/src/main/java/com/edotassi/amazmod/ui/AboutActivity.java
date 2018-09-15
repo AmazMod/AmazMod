a7e06bf8dacad8a7fb38119d42e8279cd8eb161f
LFOM <lfom@gmx.com.br>
2018-09-15 12:21:49 -0300

    Standard Amazfit notification test added to About activity

--- app/src/main/java/com/edotassi/amazmod/ui/AboutActivity.java
+++ app/src/main/java/com/edotassi/amazmod/ui/AboutActivity.java
@@ -1,11 +1,17 @@
 package com.edotassi.amazmod.ui;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.service.notification.StatusBarNotification;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -18,9 +24,22 @@ import com.edotassi.amazmod.R;
 import com.edotassi.amazmod.watch.Watch;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.huami.watch.notification.data.StatusBarNotificationData;
+import com.huami.watch.transport.DataBundle;
+import com.huami.watch.transport.DataTransportResult;
+import com.huami.watch.transport.Transporter;
+import com.huami.watch.transport.TransporterClassic;
 import com.mikepenz.iconics.context.IconicsLayoutInflater2;
 import com.pixplicity.easyprefs.library.Prefs;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import amazmod.com.models.Reply;
 import amazmod.com.transport.data.NotificationData;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -75,6 +94,10 @@ public class AboutActivity extends AppCompatActivity {
                 sendTestMessage('S');
                 break;
             }
+            case (R.id.action_activity_about_notification_test): {
+                sendTestMessage('N');
+                break;
+            }
             case (R.id.action_activity_about_revoke_device_owner): {
                 sendTestMessage('R');
                 break;
@@ -104,6 +127,12 @@ public class AboutActivity extends AppCompatActivity {
                 notificationData.setText("Test Notification");
                 break;
             }
+            case ('N'): {
+                notificationData.setForceCustom(false);
+                notificationData.setText("Test Notification");
+                sendNotificationWithStandardUI(notificationData);
+                return;
+            }
             case ('R'): {
                 notificationData.setForceCustom(false);
                 notificationData.setText("Revoke Admin Owner");
@@ -123,7 +152,7 @@ public class AboutActivity extends AppCompatActivity {
         notificationData.setTitle("AmazMod");
         notificationData.setTime("00:00");
         notificationData.setVibration(Integer.valueOf(Prefs.getString(Constants.PREF_NOTIFICATIONS_VIBRATION, Constants.PREF_DEFAULT_NOTIFICATIONS_VIBRATION)));
-        notificationData.setHideReplies(true);
+        notificationData.setHideReplies(Prefs.getBoolean(Constants.PREF_DISABLE_NOTIFICATIONS_REPLIES, Constants.PREF_DEFAULT_DISABLE_NOTIFICATIONS_REPLIES));
         notificationData.setHideButtons(false);
 
         try {
@@ -166,21 +195,92 @@ public class AboutActivity extends AppCompatActivity {
             }
         });
     }
-/*
-    private void sendNotificationWithStandardUI(byte filterResult, StatusBarNotification statusBarNotification) {
-        DataBundle dataBundle = new DataBundle();
 
-        if (filterResult == Constants.FILTER_UNGROUP && Prefs.getBoolean(Constants.PREF_NOTIFICATIONS_ENABLE_UNGROUP, false)) {
-            int nextId = (int) (long) (System.currentTimeMillis() % 10000L);
-            StatusBarNotification sbn = new StatusBarNotification(statusBarNotification.getPackageName(), "",
-                    statusBarNotification.getId() + nextId,
-                    statusBarNotification.getTag(), 0, 0, 0,
-                    statusBarNotification.getNotification(), statusBarNotification.getUser(),
-                    statusBarNotification.getPostTime());
-            dataBundle.putParcelable("data", StatusBarNotificationData.from(this, sbn, false));
-        } else {
-            dataBundle.putParcelable("data", StatusBarNotificationData.from(this, statusBarNotification, false));
+    private void sendNotificationWithStandardUI(NotificationData nd) {
+
+        DataBundle dataBundle = new DataBundle();
+        Intent intent = new Intent();
+        int nextId = (int) (long) (System.currentTimeMillis() % 10000L);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Bitmap pic = BitmapFactory.decodeResource(getResources(), R.drawable.art_canteen_intro1);
+
+        Intent buttonIntent = new Intent(getBaseContext(), AboutActivity.class);
+        buttonIntent.putExtra("notificationId", nextId);
+        PendingIntent dismissIntent = PendingIntent.getBroadcast(getBaseContext(), 0, buttonIntent, 0);
+
+        NotificationCompat.Action action2 =
+                new NotificationCompat.Action.Builder(android.R.drawable.ic_delete, "Dismiss", dismissIntent)
+                        .build();
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(NotificationCompat.EXTRA_LARGE_ICON_BIG, pic);
+        bundle.putParcelable(NotificationCompat.EXTRA_LARGE_ICON, pic);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "")
+                .setSmallIcon(R.drawable.ic_launcher_round)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .addExtras(bundle)
+                .addAction(android.R.drawable.ic_delete, "DISMISS", dismissIntent)
+                .setContentIntent(pendingIntent)
+                .setContentText(nd.getText())
+                .setContentTitle("Test");
+
+        final String INTENT_ACTION_REPLY = "com.amazmod.action.reply";
+        final String EXTRA_REPLY = "extra.reply";
+        final String EXTRA_NOTIFICATION_KEY = "extra.notification.key";
+        final String EXTRA_NOTIFICATION_ID = "extra.notification.id";
+
+
+        List<Reply> repliesList = loadReplies();
+
+        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender()
+                .setContentIcon(R.drawable.ic_launcher_round)
+                .setContentIntentAvailableOffline(true)
+                .addAction(action2)
+                .setBackground(pic);
+        for (Reply reply : repliesList) {
+            intent.setPackage("com.amazmod.service");
+            intent.setAction(INTENT_ACTION_REPLY);
+            intent.putExtra(EXTRA_REPLY, reply.getValue());
+            intent.putExtra(EXTRA_NOTIFICATION_KEY, "0|com.edotassi.amazmod|" + String.valueOf(nextId + 1) + "|tag|0");
+            intent.putExtra(EXTRA_NOTIFICATION_ID, nextId + 1);
+            PendingIntent replyIntent = PendingIntent.getBroadcast(this, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_ONE_SHOT);
+
+            NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(android.R.drawable.ic_input_add, reply.getValue(), replyIntent).build();
+            wearableExtender.addAction(replyAction);
         }
+
+        builder.extend(wearableExtender);
+        Notification notification = builder.build();
+/*
+        notification.extras.getBundle("android.wearable.EXTENSIONS");
+
+        if (notification.extras == null) {
+            Bundle bundle = new Bundle();
+            notification.extras.putBundle("android.car.EXTENSIONS", bundle);
+        }
+*/
+        StatusBarNotification sbn = new StatusBarNotification("com.edotassi.amazmod", "",
+                nextId+1,"tag", 0, 0, 0,
+                notification, android.os.Process.myUserHandle(),
+                System.currentTimeMillis());
+
+        StatusBarNotificationData sbnd = StatusBarNotificationData.from(this, sbn, false);
+
+        dataBundle.putParcelable("data", sbnd);
+/*
+        StatusBarNotificationData sbnd2 = dataBundle.getParcelable("notification");
+        System.out.println("AmazMod AboutActivity StandardUI sbnd: " + sbnd);
+
+        //NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        //notificationManagerCompat.notify(123456, sbnd2.notification.originalNotification);
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("notification", sbn);
+*/
 
         //Connect transporter
         Transporter notificationTransporter = TransporterClassic.get(this, "com.huami.action.notification");
@@ -189,13 +289,24 @@ public class AboutActivity extends AppCompatActivity {
         notificationTransporter.send("add", dataBundle, new Transporter.DataSendResultCallback() {
             @Override
             public void onResultBack(DataTransportResult dataTransportResult) {
-                log.d(dataTransportResult.toString());
+                System.out.println("AmazMod AboutActivity dataTransportResult: " + dataTransportResult.toString());
             }
         });
 
         //Disconnect transporter to avoid leaking
         notificationTransporter.disconnectTransportService();
 
-        log.i("NotificationService StandardUI: " + dataBundle.toString());
-    } */
+    }
+
+    private List<Reply> loadReplies() {
+        final String replies = Prefs.getString(Constants.PREF_NOTIFICATIONS_REPLIES, "[]");
+
+        try {
+            Type listType = new TypeToken<List<Reply>>() {
+            }.getType();
+            return new Gson().fromJson(replies, listType);
+        } catch (Exception ex) {
+            return new ArrayList<>();
+        }
+    }
 }
