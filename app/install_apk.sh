1a3926030af0c294a7b12676339335447d3d7726
lfom <lfom@gmx.com.br>
2018-11-03 06:13:17 -0300

    Update install_apk.sh

--- app/install_apk.sh
+++ app/install_apk.sh
@@ -5,7 +5,7 @@ systype=$(getprop | grep display.id)
 echo "Date: $(date)"
 echo "System: $systype"
 echo "PWD: $PWD"
-if [ "$1" != "" ]; then
+if [ "$1" != "" && "$1" != "&" ]; then
    adb kill-server
    echo "installing: $1"
    adb shell pm install -r $1
@@ -17,10 +17,11 @@ echo "launcher restarted"
 sleep 3
 adb shell dpm set-device-owner com.amazmod.service/.AdminReceiver
 echo "device ownner set"
+adb kill-server
+echo "removing files from internal storage"
 rm /sdcard/install_apk.sh
-rm $1
+rm /sdcard/AmazMod-service-*.apk
 echo "installation finished"
 } | busybox tee /dev/tty | while read line; do
    log -p d -t "$tag" "$line"
 done
-adb kill-server
