6e284a5fc39e8b28c4f34183aaf36b76d21c5367
lfom <lfom@gmx.com.br>
2018-11-03 03:12:57 -0300

    Update install_apk.sh

--- app/install_apk.sh
+++ app/install_apk.sh
@@ -2,18 +2,22 @@
 tag="AmazMod install_apk"
 systype=$(getprop | grep display.id)
 {
+adb kill-server
 date
 echo "System: $systype"
 echo "PWD: $PWD"
 echo "installing: $1"
-adb install -r $1
+adb pm install -r $1
 echo "$1 installed"
 sleep 3
+adb kill-server
 adb shell am force-stop com.huami.watch.launcher
 echo "launcher restarted"
 sleep 3
+adb kill-server
 adb shell dpm set-device-owner com.amazmod.service/.AdminReceiver
 echo "device ownner set"
+adb kill-server
 rm /sdcard/install_apk.sh
 rm $1
 echo "installation finished"
