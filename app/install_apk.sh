49e2dc887cb9a55bee5a9132af3cf367f82fcb50
lfom <lfom@gmx.com.br>
2018-11-03 04:02:43 -0300

    Update install_apk.sh

--- app/install_apk.sh
+++ app/install_apk.sh
@@ -1,13 +1,11 @@
 #/system/bin/sh
 if ["$2" == ""]; then
    adb kill-server
-   busybox nohup sh /sdcard/install_apk.sh $1 OK &
-   adb shell am force-stop com.amazmod.service
+   adb shell "busybox nohup sh /sdcard/install_apk.sh $1 OK &"
 fi  
 tag="AmazMod install_apk"
 systype=$(getprop | grep display.id)
 {
-adb kill-server
 date
 echo "System: $systype"
 echo "PWD: $PWD"
@@ -15,17 +13,15 @@ echo "installing: $1"
 adb shell pm install -r $1
 echo "$1 installed"
 sleep 3
-adb kill-server
 adb shell am force-stop com.huami.watch.launcher
 echo "launcher restarted"
 sleep 3
-adb kill-server
 adb shell dpm set-device-owner com.amazmod.service/.AdminReceiver
 echo "device ownner set"
-adb kill-server
 rm /sdcard/install_apk.sh
 rm $1
 echo "installation finished"
 } | busybox tee /dev/tty | while read line; do
    log -p d -t "$tag" "$line"
 done
+adb kill-server
