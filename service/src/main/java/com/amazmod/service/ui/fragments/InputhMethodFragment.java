8f2856987153777da7105b8f7727a98f8e69601e
Edoardo Tassinari <edo.tassinari@gmail.com>
2018-11-22 22:47:57 +0100

    add 1760 service

--- service/src/main/java/com/amazmod/service/ui/fragments/InputhMethodFragment.java
+++ service/src/main/java/com/amazmod/service/ui/fragments/InputhMethodFragment.java
@@ -57,6 +57,7 @@ public class InputhMethodFragment extends Fragment {
 
     }
 
+    /*
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
@@ -64,8 +65,8 @@ public class InputhMethodFragment extends Fragment {
 
         return inflater.inflate(R.layout.activity_radiobutton, container, false);
     }
+    */
 
-    /*
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -74,7 +75,6 @@ public class InputhMethodFragment extends Fragment {
         updateContent();
 
     }
-    */
 
     @Override
     public void onStart() {
@@ -83,7 +83,7 @@ public class InputhMethodFragment extends Fragment {
     }
 
     private void updateContent(){
-
+/*
         title = getActivity().findViewById(R.id.radio_textView01);
         title.setText(getResources().getString(R.string.input_methods));
 
@@ -133,7 +133,7 @@ public class InputhMethodFragment extends Fragment {
             });
 
         }
-
+*/
     }
 
     private void runCommand(String command) {
