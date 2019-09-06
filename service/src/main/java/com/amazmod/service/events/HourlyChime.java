5f5d055791a7e2aa6205a9104029eba3baf032ce
fcaronte <fra.caronte@gmail.com>
2019-09-06 22:27:05 +0200

    WIP - hourly chime - now is not need to use adb to change is state and miss only to do the initial check of the setting

--- service/src/main/java/com/amazmod/service/events/HourlyChime.java
+++ service/src/main/java/com/amazmod/service/events/HourlyChime.java
@@ -6,24 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
-
 import com.amazmod.service.receiver.AlarmReceiver;
-
 import org.tinylog.Logger;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Calendar;
 
 public class HourlyChime {
-
-    public static void setHourlyChime(Context context) {
-        // Automatic enable/disable
-        boolean status = !HourlyChime.checkIfSet();
-        setHourlyChime(context, status);
-    }
-
     public static void setHourlyChime(Context context, Boolean status) {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(NotificationCompat.CATEGORY_ALARM);
         Intent intent = new Intent(context, AlarmReceiver.class);
@@ -48,25 +35,5 @@ public class HourlyChime {
             alarmMgr.cancel(alarmIntent);
             Logger.info("setHourlyChime canceled");
         }
-    }
-
-    public static boolean checkIfSet(){
-        try {
-            Process process = Runtime.getRuntime().exec("adb shell dumpsys alarm");
-            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = null;
-
-            while ((line = in.readLine()) != null) {
-                if (line.contains("com.amazmod.service/.receiver.AlarmReceiver")) {
-                    Logger.info("Alarm is set");
-                    return true;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Logger.info("Alarm is not set");
-        return false;
     }
 }
