e08539e1796bed4d656f19f66277b7b7069c4880
fcaronte <fra.caronte@gmail.com>
2020-01-03 21:29:31 +0100

    Test to add LocaleUtils to can use more languages

--- service/src/main/java/com/amazmod/service/AmazModService.java
+++ service/src/main/java/com/amazmod/service/AmazModService.java
@@ -2,18 +2,29 @@ package com.amazmod.service;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+
+import androidx.annotation.NonNull;
 
 import com.amazmod.service.db.model.BatteryDbEntity;
 import com.amazmod.service.db.model.BatteryDbEntity_Table;
+import com.amazmod.service.util.LocaleUtils;
+import com.pixplicity.easyprefs.library.Prefs;
 import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 import org.tinylog.Logger;
 import org.tinylog.configuration.Configuration;
 
+import java.util.Locale;
+
 public class AmazModService extends Application {
 
+    public static Locale defaultLocale;
     private static Application mApp;
     private static String level;
 
@@ -25,6 +36,17 @@ public class AmazModService extends Application {
 
         setupLogger();
 
+        setupLocale();
+
+/*      //THIS ONE IS WORK ALONE WITHOUT NEED   PROTECTED VOID ATTACHBASECONTEXT, PRIVATE VOID SETUPLOCALE(), AND THE LOCATEUTILS BUT CAN USE ONLY ONE LANG SET ON "CONF.LOCATE" LITE
+
+        Resources res = getContext().getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.locale = Locale.ITALY;
+        //conf.setLayoutDirection(locale);
+        res.updateConfiguration(conf, dm);
+*/
         //EventBus.getDefault().init(this);
         Logger.info("Tinylog configured debug: {} level: {}",
                 (BuildConfig.VERSION_NAME.toLowerCase().contains("dev"))?"TRUE":"FALSE", level.toUpperCase());
@@ -35,6 +57,21 @@ public class AmazModService extends Application {
         startService(new Intent(this, MainService.class));
 
         Logger.info("AmazModService onCreate");
+    }
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        new Prefs.Builder()
+                .setContext(context)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setUseDefaultSharedPreference(true)
+                .build();
+        super.attachBaseContext(LocaleUtils.onAttach(context));
+        Logger.debug("AmazModApplication attachBaseContext");
+    }
+
+    private void setupLocale() {
+        defaultLocale = LocaleUtils.getLocale();
     }
 
     private static void cleanOldBatteryDb() {
