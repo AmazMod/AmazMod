24022bf0a5942c29428c20370a0ad01dcc784567
Marcelo Diotto <diotto@gmail.com>
2020-05-25 23:40:10 -0300

    Small Improvement to Kotlin Code and Fix to Add button

--- app/src/main/java/com/edotassi/amazmod/ui/NotificationRepliesDragActivity.kt
+++ app/src/main/java/com/edotassi/amazmod/ui/NotificationRepliesDragActivity.kt
@@ -6,8 +6,6 @@ import android.text.InputType
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import butterknife.ButterKnife
-import butterknife.OnClick
 import com.afollestad.materialdialogs.MaterialDialog
 import com.edotassi.amazmod.R
 import com.edotassi.amazmod.adapters.NotificationRepliesAdapter
@@ -30,10 +28,9 @@ class NotificationRepliesDragActivity : BaseAppCompatActivity() {
         } catch (exception: NullPointerException) {
             KtLogger.error("AboutActivity onCreate exception: " + exception.message)
         }
-        ButterKnife.bind(this)
         val mLayoutManager = LinearLayoutManager(this)
         mLayoutManager.orientation = LinearLayoutManager.VERTICAL
-        activity_notification_replies_list.setLayoutManager(mLayoutManager)
+        replies_list.layoutManager = mLayoutManager
         val callback: DynamicEventsCallback = object : DynamicEventsCallback {
             override fun onItemMove(initialPosition: Int, finalPosition: Int) {
                 mAdapter.onItemMove(initialPosition, finalPosition)
@@ -44,19 +41,27 @@ class NotificationRepliesDragActivity : BaseAppCompatActivity() {
             }
         }
         val androidItemTouchHelper = ItemTouchHelper(DynamicEventsHelper(callback))
-        androidItemTouchHelper.attachToRecyclerView(activity_notification_replies_list)
+        androidItemTouchHelper.attachToRecyclerView(replies_list)
         mAdapter = NotificationRepliesAdapter(this, androidItemTouchHelper)
-        activity_notification_replies_list.adapter = mAdapter
-        activity_notification_replies_list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        replies_list.adapter = mAdapter
+        replies_list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
                 if (!recyclerView.canScrollVertically(1) && recyclerView.canScrollVertically(-1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    activity_notification_replies_add.hide()
+                    reply_add.hide()
                 } else {
-                    activity_notification_replies_add.show()
+                    reply_add.show()
                 }
             }
         })
+
+        reply_add.setOnClickListener {
+            MaterialDialog.Builder(this)
+                    .title(R.string.add_new_reply)
+                    .content(R.string.enter_the_text_you_want_as_an_answer)
+                    .inputType(InputType.TYPE_CLASS_TEXT)
+                    .input("", "") { dialog, input -> mAdapter.addItem(input.toString()) }.show()
+        }
     }
 
     override fun onPause() {
@@ -67,14 +72,5 @@ class NotificationRepliesDragActivity : BaseAppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
-    }
-
-    @OnClick(R.id.activity_notification_replies_add)
-    fun onAddReply() {
-        MaterialDialog.Builder(this)
-                .title(R.string.add_new_reply)
-                .content(R.string.enter_the_text_you_want_as_an_answer)
-                .inputType(InputType.TYPE_CLASS_TEXT)
-                .input("", "") { dialog, input -> mAdapter.addItem(input.toString()) }.show()
     }
 }
