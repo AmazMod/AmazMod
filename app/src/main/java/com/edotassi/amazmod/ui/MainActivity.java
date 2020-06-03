f85789f534672ce4c9c9a4e1cd184111eaeba5f3
Marcelo Diotto <diotto@gmail.com>
2020-06-02 23:45:31 -0300

    Removed ButterKnife from StatsActivity and MainActivity

--- app/src/main/java/com/edotassi/amazmod/ui/MainActivity.java
+++ app/src/main/java/com/edotassi/amazmod/ui/MainActivity.java
@@ -54,7 +54,6 @@ import java.util.Locale;
 import java.util.Objects;
 
 import amazmod.com.transport.Constants;
-import butterknife.ButterKnife;
 
 public class MainActivity extends BaseAppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -89,8 +88,6 @@ public class MainActivity extends BaseAppCompatActivity
             setTheme(R.style.AppTheme_NoActionBar);
             setContentView(R.layout.activity_main);
         }
-
-        ButterKnife.bind(this);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
